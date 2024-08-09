f3e17802bae1bb5317f43ed768346ee28cab17a8
deepakkumar <deepakkannan2914@gmail.com>
2024-08-09 14:08:15 +0530

    commit message

---
+++ index.js
@@ -0,0 +1,68 @@
+// function add(){
+//     date=document.getElementById("de").style.color="green"
+//     document.innerText="idhu epdi iruku"
+//     // date.innerHTML="bye"
+//     // date.onmouseover
+// }
+// function dlt(){
+//      date=document.getElementById("de").style.color="blue"
+// }
+
+
+// dele=()=>{
+//         const h=document.getElementById("open")
+//         document.body.removeChild(h)
+// }
+// function add(){
+//     const j=document.getElementById("data")
+//     const h=document.getElementById("i")
+//     h.removeChild(h.children[1])
+// }
+
+// function add(){
+//     const j=document.getElementsByid("data")
+//     const h=document.getElementById("i") 
+// }
+// console.log(h[0].innerHTML)
+
+// function add(){
+//     const b=document.getElementById("c")
+//     for( let i=1;i<b.length;i++){
+//         b[i].style.color="blue"
+//     }
+// }
+
+
+// function evalve(){
+//     let a=document.getElementById("adds").value;
+//     console.log(a);
+//     let b=eval(a);
+//     document.getElementById("results").innerHTML=b;
+// }
+
+
+// const h=document.getElementById("hlo")
+// h.addEventListener("click",function(){
+//     const para=document.createElement("p")
+//     para.innerHTML="hello";
+//     document.body.appendChild(para)
+// })
+
+
+// $(document).ready(function(){
+//     $("#hlo").hide()
+// })
+
+// get
+$(document).ready(function(){
+    $("#hlo").click(function(){
+        $("p").html(" will do whatever I say")
+    })
+})
+
+$(document).ready(function(){
+    $("#hlo").click(function(){
+        const data=$("input").val()
+        
+    })
+})
